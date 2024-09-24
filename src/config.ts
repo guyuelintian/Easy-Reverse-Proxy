a79a56ec773cd909890e5275fb16242c483b8cde
gywl <104403232+guyuelintian@users.noreply.github.com>
2024-09-24 22:28:53 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,6 @@ export interface Proxy {
 
 // 代理
 export let PROXYS = [{
-        domain: "easy-reverse-proxy-qsjo.vercel.app",
+        domain: "easy-reverse-proxy-three.vercel.app",
         url: "https://www.google.com/",
     }];
